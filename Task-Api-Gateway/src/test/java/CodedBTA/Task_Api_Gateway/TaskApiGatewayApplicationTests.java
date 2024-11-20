7e67dd802c9ca1e0fa40bc18ea7a4e8bdd29864f
Abdulrahman Alfahad <abdulrahman.g.alfahad@gmail.com>
2024-11-20 09:05:05 +0300

    Completed the task

---
+++ Task-Api-Gateway/src/test/java/CodedBTA/Task_Api_Gateway/TaskApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package CodedBTA.Task_Api_Gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
